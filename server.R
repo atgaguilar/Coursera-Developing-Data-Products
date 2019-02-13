eabd95b649e8471bc06aaaea064808a5e3ab0127
atgaguilar <33001070+atgaguilar@users.noreply.github.com>
2019-02-14 07:53:38 +0800

    Add files via upload

---
+++ server.R
@@ -0,0 +1,27 @@
+
+library(shiny)
+library(plotly)
+library(ggplot2)
+
+
+dataset<-read.csv("pnr_monthlypassengersperstation2017.csv", sep=",", header=T)
+
+shinyServer(function(input,output){
+  
+  output$month <- renderText(input$month)
+  
+
+  output$average <- renderPrint({
+    mean(dataset[[input$month]])
+  })
+  
+  output$plot1 <- renderPlotly({
+    
+ plot_ly(data=dataset, x=dataset$Station, y=dataset[[input$month]],  type="bar") %>% 
+      layout(title="Passenger Traffic" , xaxis=list(title="Station"), yaxis=list(title="Number of Passengers"))
+
+
+  })
+  
+})
+
